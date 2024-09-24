d706f9899344a9c59843efff283c70eb5630fb69
Wonhwa.Jung <john.wick@sinsabridge.com>
2024-09-24 11:35:28 +0900

    react 연습

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+const reactDescritions = ['기초', 'Crucial', 'Core'];
+
+function getRandomInt(max){
+  return Math.floor(Math.random()*(max + 1));
+}
+
+function App() {
+  const descritons = reactDescritions[getRandomInt(2)];
+  return (
+    <div>
+      <header>
+        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+        <h1>React Essentials</h1>
+        <p>
+          {descritons} React concepts you will need for almost any app you are
+          going to build!
+        </p>
+      </header>
+      <main>
+        <h2>Time to get started!</h2>
+      </main>
+    </div>
+  );
+}
+
+export default App;
